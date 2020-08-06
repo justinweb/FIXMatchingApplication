8be9a3daa6efbd3872e231807c2c667d8f4f717c
justin <hwuehwen.lin@kgi.com>
2020-08-06 14:27:46 +0800

    first version

---
+++ FIXMatchingServer/FIXMatchingApplication.cs
@@ -0,0 +1,49 @@
+﻿using QuickFix;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIXMatchingServer
+{
+    public class FIXMatchingApplication : IApplication
+    {
+        public void FromAdmin(Message message, SessionID sessionID)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void FromApp(Message message, SessionID sessionID)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("FromApp\r\n");
+        }
+
+        public void OnCreate(SessionID sessionID)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnLogon(SessionID sessionID)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnLogout(SessionID sessionID)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void ToAdmin(Message message, SessionID sessionID)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void ToApp(Message message, SessionID sessionID)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("ToApp\r\n");
+        }
+    }
+}
---
+++ FIXMatchingServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using QuickFix;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIXMatchingServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cfgFile = ConfigurationManager.AppSettings["Configure"].ToString();
+            SessionSettings settings = new SessionSettings( cfgFile );
+            IApplication app = new FIXMatchingApplication();
+            IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+            ILogFactory logFactory = new FileLogFactory(settings);
+            IAcceptor acceptor = new ThreadedSocketAcceptor(app, storeFactory, settings, logFactory);
+
+            acceptor.Start();
+            Console.WriteLine("press <enter> to quit");
+            Console.Read();
+            acceptor.Stop();
+        }
+    }
+}
